35164cf06f566a1de38314588e5925682eb96c75
fcoisaias <65827476+fcoisaias@users.noreply.github.com>
2022-08-02 14:18:49 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,57 @@
 let veces = 0
+let X = 0
+let Y = 0
 function contar () {
     while (veces >= 0) {
         basic.showNumber(veces)
         veces = veces - 1
     }
 }
-basic.forever(function () {
+function CAMINITO () {
+    X = 0
+    Y = 0
+    while (Y <= 4) {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+        Y += 1
+        basic.clearScreen()
+    }
+    Y = 0
+    for (let index = 0; index < 4; index++) {
+        while (Y <= 4) {
+            for (let X = 0; X <= 4; X++) {
+                led.plot(X, Y)
+                basic.pause(100)
+            }
+            Y += 1
+        }
+    }
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+}
+function _0Y1 () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+    }
+}
+function basico () {
     veces = 5
     contar()
     for (let index = 0; index < 20; index++) {
@@ -26,4 +72,12 @@ basic.forever(function () {
         basic.showIcon(IconNames.Sad)
         basic.pause(500)
     }
+    contar()
+}
+basic.forever(function () {
+    basico()
+    CAMINITO()
+    contar()
+    _0Y1()
+    contar()
 })
